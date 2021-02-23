aab7f750a1db5ec9d4fb93d1c3997ebd8ccc4eb6
raedgzmn <deguzmanr652@gmail.com>
2021-02-23 17:11:15 +0800

    changes

--- script.js
+++ script.js
@@ -45,6 +45,7 @@ const switchPlayer = function() {
         activePlayer = activePlayer === 0 ? 1 : 0;
         player0El.classList.toggle('player--active');
         player1El.classList.toggle('player--active');
+        
 }
 
 // Rolling dice function
